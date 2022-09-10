f4b5944aee633dd2ec697639b398c7794ddedf39
Murat-Selim <66679051+Murat-Selim@users.noreply.github.com>
2022-09-10 22:06:29 +0300

    first commit

---
+++ firstCase.js
@@ -0,0 +1,9 @@
+   const number = 243
+   const stepsTotal = 2 + 4 + 3
+   if(stepsTotal % 2 == 0) {
+    console.log(`${result} is even`)
+   }
+   else{
+    console.log(`${result} is odd`)
+   }
+
