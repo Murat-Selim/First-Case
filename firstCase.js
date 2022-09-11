2be20e0e34282e5eaafbcf6969c46f03a8bb1e08
Murat-Selim <66679051+Murat-Selim@users.noreply.github.com>
2022-09-11 13:23:56 +0300

    update

--- firstCase.js
+++ firstCase.js
@@ -1,9 +1,9 @@
    const number = 243
    const stepsTotal = 2 + 4 + 3
-   if(stepsTotal % 2 == 0) {
-    console.log(`${result} is even`)
+   if(stepsTotal % 2 == 1) {
+    console.log(`${result} is odd`)
    }
    else{
-    console.log(`${result} is odd`)
+    console.log(`${result} is even`)
    }
 
